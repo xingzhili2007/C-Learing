5847794e8783e87999588cf3b39dd6376c77fed6
xingzhili2007 <60608543+xingzhili2007@users.noreply.github.com>
2020-06-14 11:37:01 +0800

    Add files via upload

---
+++ P1164 小A点菜-洛谷_背包/main.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int INF=2147483647;
+int 
+N,//菜品种类有N种 
+M,//口袋里有M元
+dp[110][1010],
+pri[110]//价格 
+;
+int main(int argc,char *argv[])
+{
+	//Input
+	cin>>N>>M;
+	for(int i=1;i<=n;i++)
+		cin>>pri[i];
+	//Reset
+	for(int i=0;i<=n;i++) f[i][0]=
+	
+	
+	
+}
